4b3e3d1a3072d601020319d83a56380cc54e4e9f
Michael Scherbakow <ryupold@gmail.com>
2018-08-11 18:48:07 +0200

    initial room and people systems

---
+++ Jam4TheWin/Assets/Systems/Room/KeepInsideRoomComponent.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SystemBase;
+
+namespace Systems.Room
+{
+    public class KeepInsideRoomComponent : GameComponent
+    {
+        [Range(-10, 10)]
+        public float customOffsetToGround = 0f;
+    }
+}
