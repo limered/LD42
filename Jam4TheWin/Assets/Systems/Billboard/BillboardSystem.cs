4d9335b987735c99ab321890ea073022b330ed15
Michael Scherbakow <ryupold@gmail.com>
2018-08-11 16:59:17 +0200

    added Billboard stuff

---
+++ Jam4TheWin/Assets/Systems/Billboard/BillboardSystem.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using SystemBase;
+using UniRx.Triggers;
+using UniRx;
+using UnityEngine;
+
+
+namespace Systems.Billboard
+{
+    [GameSystem]
+    public class BillboardSystem : GameSystem<BillboardComponent>
+    {
+        public override void Register(BillboardComponent comp)
+        {
+            comp.UpdateAsObservable()
+                .Subscribe(_ =>
+                    {
+                        if (comp.only2DRotation)
+                        {
+                            Vector3 v = Camera.main.transform.position - comp.transform.position;
+                            v.x = v.z = 0.0f;
+                            comp.transform.LookAt(Camera.main.transform.position - v);
+                        }
+                        else
+                        {
+                            comp.transform.up = Camera.main.transform.up;
+                        }
+                    })
+                .AddTo(comp);
+        }
+    }
+}
