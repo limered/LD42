78f45163e6403ae8f67abbd9df4e3745f7a5fcbb
Emil Wasilewski <me@limered.de>
2018-08-11 16:39:08 +0200

    add  mouse interaction

---
+++ Jam4TheWin/Assets/Systems/Player/CatSystem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using SystemBase;
+using Systems.Player.Movement;
+
+namespace Systems.Player
+{
+    [GameSystem]
+    public class CatSystem : GameSystem<CatComponent, PlayerInteractionComponent>
+    {
+        private CatComponent _cat;
+
+        public override void Register(CatComponent component)
+        {
+            _cat = component;
+        }
+
+        public override void Register(PlayerInteractionComponent component)
+        {
+            
+        }
+    }
+}
---
+++ Jam4TheWin/Assets/Systems/Player/Movement/MovementSystem.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using SystemBase;
+using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+using UnityEngine.Experimental.UIElements;
+
+namespace Systems.Player.Movement
+{
+    [GameSystem(typeof(CatSystem))]
+    public class MovementSystem : GameSystem<PlayerInteractionComponent>
+    {
+        public override void Register(PlayerInteractionComponent component)
+        {
+            component.UpdateAsObservable()
+                .Subscribe(CheckMousePorition(component))
+                .AddTo(component);
+        }
+
+        private static Action<Unit> CheckMousePorition(PlayerInteractionComponent component)
+        {
+            return u =>
+            {
+                if(Input.GetMouseButtonDown((int) MouseButton.LeftMouse))
+                {
+                    component.MousePressed.Value = true;
+                }
+                if (Input.GetMouseButtonUp((int) MouseButton.LeftMouse))
+                {
+                    component.MousePressed.Value = false;
+                }
+
+                if (!Input.GetMouseButton((int) MouseButton.LeftMouse)) return;
+
+                var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+                if (!Physics.Raycast(ray, out hit, Mathf.Infinity)) return;
+
+                component.MousePosition.Value = new Vector2(hit.point.x, hit.point.z);
+                Debug.Log(component.MousePosition.Value.x + " " + component.MousePosition.Value.y);
+            };
+        }
+    }
+}
