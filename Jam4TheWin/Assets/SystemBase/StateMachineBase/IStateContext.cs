264c7a1a179e169ef68385483fdf946323c9db24
Emil Wasilewski <me@limered.de>
2018-08-11 20:13:13 +0200

    add statemachine base

---
+++ Jam4TheWin/Assets/SystemBase/StateMachineBase/IStateContext.cs
@@ -0,0 +1,10 @@
+﻿using UniRx;
+
+namespace SystemBase.StateMachineBase
+{
+    public interface IStateContext<TState> where TState : IState
+    {
+        ReactiveProperty<TState> CurrentState { get; }
+        bool GoToState(TState state);
+    }
+}
---
+++ Jam4TheWin/Assets/SystemBase/StateMachineBase/StateContextBase.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using UniRx;
+
+namespace SystemBase.StateMachineBase
+{
+    public class StateContextBase<TState> : IStateContext<TState> where TState : IState
+    {
+        public StateContextBase()
+        {
+            CurrentState = new ReactiveProperty<TState>();
+        }
+
+        public ReactiveProperty<TState> CurrentState { get; private set; }
+        public bool GoToState(TState state)
+        {
+            if (CurrentState.Value.ValidNextStates.All(st => st != state.GetType())) return false;
+            if (!CurrentState.HasValue) return false;
+
+            CurrentState.Value.Exit();
+            CurrentState.Value = state;
+            return CurrentState.Value.Enter(this);
+        }
+    }
+}
