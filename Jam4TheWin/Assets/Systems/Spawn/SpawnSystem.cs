fb0680a9b38397257d133a13e6e5b0ddd4d3c0ef
Michael Scherbakow <ryupold@gmail.com>
2018-08-12 16:13:15 +0200

    spawner and gathering points

---
+++ Jam4TheWin/Assets/Systems/Spawn/SpawnSystem.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using SystemBase;
+using UniRx.Triggers;
+using UniRx;
+using UnityEngine;
+using Utils.Plugins;
+using System.Linq;
+using System;
+
+namespace Systems.Room
+{
+    [GameSystem]
+    public class SpawnSystem : GameSystem<SpawnerComponent>
+    {
+        public override void Register(SpawnerComponent comp)
+        {
+            comp.Interval
+            .SelectMany(x => Observable.Interval(TimeSpan.FromSeconds(comp.Interval.Value)).TakeUntil(comp.Interval.Skip(1).Take(1)))
+            .Where(_ => comp.Spawns != null)
+            .Subscribe(_ => GameObject.Instantiate(comp.Spawns, comp.transform.position, Quaternion.identity))
+            .AddTo(comp);
+        }
+    }
+}
---
+++ Jam4TheWin/Assets/Systems/Spawn/SpawnerComponent.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SystemBase;
+using UniRx;
+
+namespace Systems.Room
+{
+    public class SpawnerComponent : GameComponent
+    {
+        public GameObject Spawns;
+        public FloatReactiveProperty Interval = new FloatReactiveProperty(5);
+    }
+}
