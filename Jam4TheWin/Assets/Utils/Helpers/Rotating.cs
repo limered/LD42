8c8ddf53a9e432e44d8c7edafbd1eb6949ff65fa
Michael Scherbakow <ryupold@gmail.com>
2018-08-13 17:31:20 +0200

    stinky stuff

---
+++ Jam4TheWin/Assets/Utils/Helpers/Rotating.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotating : MonoBehaviour 
+{
+	public float rotationSpeed = 1;
+	public float angle;	
+
+	void Start () {
+		angle = transform.rotation.eulerAngles.y;
+	}
+	
+	void Update () {
+		angle = (angle + rotationSpeed * Time.deltaTime) % 360;
+		 
+		transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, angle, transform.rotation.eulerAngles.z);
+	}
+}
